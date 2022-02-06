c7d2e46496dc3311bf416538abc184f0e43e8e92
凯 - kǎi <51862296+KaiBelmo@users.noreply.github.com>
2022-02-06 21:29:36 +0800

    Update main.c

--- vector/main.c
+++ vector/main.c
@@ -6,7 +6,15 @@ int8_t compare(void *a, void *b){
     return strcmp((char *)a, (char *)b);
 }
 
+void print(vector *vec){
+    for (int i = 0; i < vec->len; i++){
+        printf("%s ", (char *)vec->data[i]);
+    }
+    printf("\n");
+}
+
 int main(){
+    int idx = 0;
     vector *vec_a = vec_new(50, sizeof(void *), true);
     vec_push_back(vec_a, "aaa");
     vec_push_back(vec_a, "bbb");
@@ -17,12 +25,17 @@ int main(){
     vec_push_back(vec_a, "ggg");
     vec_push_back(vec_a, "hhh");
     vec_push_back(vec_a, "iii");
-    // for (int i = 0; i < vec_a->len; i++){
-        // printf("%s ", (char *)vec_a->data[i]);
-    // }
-    // printf("%d | %s", vec_find(vec_a, "iii", 0, vec_a->len - 1, compare), (char *)vec_a->data[vec_find(vec_a, "iii", 0, vec_a->len - 1, compare)]);
-    printf("%d\n", vec_find_s(vec_a, "zzz", 0, vec_a->len - 1, compare));
-    // printf("%d\n", vec_find(vec_a, "zzz", compare));
-
+    printf("vec_a capacity is %d | vec_a len is %ld\n", vec_capacity(vec_a), vec_size(vec_a));
+    print(vec_a);
+    if ((idx = vec_find_s(vec_a, vec_back(vec_a), 0, vec_a->len - 1, compare)) != -1)
+        printf("element found, idx = %d\n", idx);
+    vec_pop_back(vec_a);
+    vec_pop_back(vec_a);
+    vec_pop_back(vec_a);
+    vec_shrink_to_fit(vec_a);
+    printf("vec_a capacity is %d | vec_a len is %ld\n", vec_capacity(vec_a), vec_size(vec_a));
+    print(vec_a);
+    if ((idx = vec_find(vec_a, "hhh", compare)) == -1)
+        printf("element not found, idx = %d\n", idx);
     return 0;
 }
